670dfe428db97c9763d56e1999ac0f79e0cca061
FaragauDan <faragaudan03@gmail.com>
2023-01-12 19:03:08 +0200

    add hide function

---
+++ examples.js
@@ -0,0 +1,18 @@
+console.info("page loaded");
+      console.debug(2 + 3);
+      console.warn(2 * (3 + 1));
+      var employed = false;
+      console.error(employed);
+      var age = 26; // number
+
+      var user = "Danny";
+      user = user + "Faragau";
+
+      var skills = { "html", "css"};
+
+      var dan = {
+          age: 26,
+          employed: true
+      };
+
+      var motto = document.getElementById("job-title")
--- index.js
+++ index.js
@@ -1,59 +1,53 @@
 var skillsEl = document.getElementById("skills-list");
 
-
 var skills = ["HTML", "CSS", "JS", "Drive"];
-
 
 var skillsHTML = "";
 var i = 0;
 
-
-for (var i = 0; i < 4;) {
-    skillsHTML += "<li>" + skilss[i] + "</li>";
-    i++
+for (var i = 0; i < 4; ) {
+  skillsHTML += "<li>" + skills[i] + "</li>";
+  i++;
 }
 
 console.info(skillsHTML);
+
 skillsEl.innerHTML = skillsHTML;
 
 //display "Home"
 
 function hideAllPages() {
-    document.getElementById("skills").style.display = "none";
-    document.getElementById("skills").style.display = "none";
-    document.getElementById("languages").style.display = "none";
-    document.getElementById("project").style.display = "none";
-    document.getElementById("home").style.display = "none";
+  hide("skills");
+  hide("languages");
+  hide("projects");
+  hide("home");
 }
 
+function show(id) {
+  document.getElementById(id).style.display = "block";
+}
 
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
 
-function dislpayHome() {
-    hideAllPages();
-    document.getElementById("home").style.display = "block";
+function displayHome() {
+  hideAllPages();
+  show("home");
 }
 function displaySkills() {
-    console.warn("trebuie sa afisam skills page");
-    hideAllPages();
-    document.getElementById("skills").style.display = "block";
+  console.warn("trebuie sa afisam skills page");
+  hideAllPages();
+  show("skills");
 }
 function displayLanguages() {
-    hideAllPages();
-    document.getElementById("languages").style.display = "block";
+  hideAllPages();
+  show("languages");
 }
 
-function displayProject() {
-    hideAllPages();
-    document.getElementById("project").style.display = "block";
+function displayProjects() {
+  hideAllPages();
+  show("projects");
 }
 
 displayHome();
-
-
-
-
-
-
-
-
-
