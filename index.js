09195a48b88acc5520ae9550e4341c6afb824e45
FaragauDan <faragaudan03@gmail.com>
2023-01-26 21:31:01 +0200

    convert function in arrow

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const r1 = fetch("skills.json");
-r1.then(function (raspuns) {
+r1.then((raspuns) => {
   const r2 = raspuns.json();
-  r2.then(function (skills) {
+  r2.then((skills) => {
     displaySkills(skills);
   });
 });
 
 function displaySkills(skills) {
-  const skillsHTML = skills.map(function (skill) {
+  const skillsHTML = skills.map((skill) => {
     return `<li>${skill.name} - <span>${skill.endorcements}</span></li>`;
   });
 
@@ -20,7 +20,7 @@ var pageIds = ["home", "skills", "languages", "projects", "rubik"];
 
 function hideAllPages() {
   const pages = document.querySelectorAll(".page");
-  pages.forEach(function (page) {
+  pages.forEach((page) => {
     console.info("inside pages.forEach", page.id);
     hide(page.id);
   });
@@ -42,17 +42,23 @@ function displayPage(id) {
 function initMenu() {
   console.warn("init meniu");
 
-  document
-    .getElementById("top-meniu-bar")
-    .addEventListener("click", function (e) {
-      if (e.target.matches("a")) {
-        // var id = e.target.getAttribute("data-page");
-        const id = e.target.dataset.page;
+  document.getElementById("top-meniu-bar").addEventListener("click", (e) => {
+    if (e.target.matches("a")) {
+      // var id = e.target.getAttribute("data-page");
+      const id = e.target.dataset.page;
 
-        displayPage(id);
-      }
-    });
+      displayPage(id);
+    }
+  });
 }
 
 displayPage("skills");
 initMenu();
+
+const f1 = function () {
+  return "arrow";
+};
+
+const f2 = () => {
+  return "arrow";
+};
